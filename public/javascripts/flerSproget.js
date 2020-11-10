1983b069ae6cb7e0f544576e1cf068d238fc552b
yokarak <54890038+yokarak@users.noreply.github.com>
2020-11-10 14:08:37 +0100

    tilføjede mulighed for at ændre placeholder i inpu

--- public/javascripts/flerSproget.js
+++ public/javascripts/flerSproget.js
@@ -11,7 +11,12 @@ function brugStrings(lang) {
     document.querySelectorAll('html [data-key]').forEach(element => {
         //for hvert data-key værdi, leder den efter det i json navngivet langdata
         let key = element.getAttribute('data-key');
-        if(key) {
+
+        if(key.includes('placeholder')) { // beregnet til input, så placeholder kan vises på dansk og engelsk
+            element.placeholder = langdata.languages[lang].strings[key]
+        }
+
+        if(!key.includes('placeholder')) {
             let temp = element.textContent;
             element.textContent = langdata.languages[lang].strings[key];
             if(!key.includes("+hbs")) { // hvis der er +hbs, så appender det ikke
